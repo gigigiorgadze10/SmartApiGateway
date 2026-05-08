a0468080d347a108b71bd9cbc2f0c49448351aaa
gigigiorgadze10 <giorgadze.gigi2004@gmail.com>
2026-05-08 23:51:41 +0400

    add migrations

---
+++ SmartApiGateway/Migrations/20260508193856_RemoveTrafficLogsFromPostgres.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SmartApiGateway.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveTrafficLogsFromPostgres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TrafficLogs");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TrafficLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndpointId = table.Column<int>(type: "integer", nullable: true),
+                    HttpMethod = table.Column<string>(type: "text", nullable: false),
+                    IpAddress = table.Column<string>(type: "text", nullable: false),
+                    RequestedUrl = table.Column<string>(type: "text", nullable: false),
+                    ResponseTimeMs = table.Column<long>(type: "bigint", nullable: false),
+                    StatusCode = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrafficLogs", x => x.Id);
+                });
+        }
+    }
+}
